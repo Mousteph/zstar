dcc011fc10fb3404be893de4fb69f846f9d2af24
Mousteph <moustephdiop@gmail.com>
2026-05-01 15:18:41 +0200

    Remove CLI support

--- zstar/__main__.py
+++ zstar/__main__.py
@@ -1,5 +1,16 @@
-from zstar.cli import app
+import sys
 
+import uvicorn
+from zstar.api.start_backend import create_app
+from zstar.config import load_config
 
 if __name__ == "__main__":
-    app()
+    config_path = sys.argv[1] if len(sys.argv) > 1 else None
+    settings = load_config(config_path)
+    app = create_app(settings)
+
+    uvicorn.run(
+        app,
+        host=settings.backend.host,
+        port=settings.backend.port,
+    )
